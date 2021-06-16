09aea096e6bc9a6d0fd906fb5d55ef25b8b833b8
Bash009 <basbashar@gmail.com>
2021-06-16 23:16:35 +0530

    added new stuff

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -3,10 +3,16 @@ import data from './data'
 function App() {
   const [count, setCount] = useState(0)
   const [text, setText] = useState([])
-
+  const handleSubmit = (e) => {
+    e.prevenDefault()
+    console.log('hello world')
+  }
   return (
     <section className='section-center'>
       <h3>tired of boring lorem ipsum?</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>paragraph:</label>
+      </form>
     </section>
   )
 }

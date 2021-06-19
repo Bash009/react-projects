8662ceebda2604c7ab4720c12375148b2ef6c15d
Bash009 <basbashar@gmail.com>
2021-06-20 01:16:55 +0530

    added new stuff

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -3,17 +3,31 @@ import data from './data'
 function App() {
   const [count, setCount] = useState(0)
   const [text, setText] = useState([])
+
   const handleSubmit = (e) => {
-    e.prevenDefault()
-    console.log('hello world')
+    e.preventDefault()
+
+    setText(data)
   }
   return (
     <section className='section-center'>
       <h3>tired of boring lorem ipsum?</h3>
       <form className='lorem-form' onSubmit={handleSubmit}>
-        <label htmlFor='amount'>paragraph:</label>
-        <input type='number' name='amount' id='amount' />
+        <label htmlFor='amount'>paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className='btn'>generate</button>
       </form>
+      <article className='lorem-text'>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+      </article>
     </section>
   )
 }
